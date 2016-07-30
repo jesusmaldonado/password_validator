0681775421ab978112b222a40eb2462c71a8b27b
jesusmaldonado <jesusmaldonado317@gmail.com>
2016-07-30 18:36:07 -0400

    broke infinite loop

--- src/services/password_service.js
+++ src/services/password_service.js
@@ -2,34 +2,25 @@ var englishWords = require("an-array-of-english-words");
 export function replaceEnglishWords(password){
   return password.replace(/([a-zA-Z]+)/g, (match) => {
     if (englishWords.includes(match.toLowerCase())) {
+      console.log(match);
       return 'a';
+    } else {
+      return match;
     }
   });
 };
 
 export function countCharacterTypes(string){
   const characterTypes = {
-    'letters': 0,
-    'digits': 0,
+    'letter': 0,
+    'digit': 0,
     'whitespace': 0,
     'other': 0
   };
   for (let idx in string) {
     const char = string[idx];
-    if (char.match(/[a-zA-Z]/g)) {
-      characterTypes['letters'] += 1;
-      continue
-    }
-    if (char.match(/[0-9]/g)) {
-      characterTypes['digits'] += 1;
-      continue
-    }
-    if (char.match(/ /g)) {
-      characterTypes['whitespace'] += 1;
-      continue
-    }
-    //if we are here we have another character
-    characterTypes['other'] += 1;
+    const charType = findType(char);
+    characterTypes[charType] += 1;
   }
   const keys = Object.keys(characterTypes);
   const characterTypeCount = keys
@@ -76,32 +67,38 @@ export function countStrength(string) {
 
 export function validatePassword(password){
   const substitutedString = replaceEnglishWords(password);
-  const passwordStrength = countStrength(substitutedString);
-  switch (passwordStrength) {
-    case (passwordStrength > 50):
-      return {
-        originalPassword: password,
-        passwordStrength,
-        strength: 'strong',
-        message: `Congratulations ${password} is strong!`
-      }
-    case (passwordStrength >= 10 && passwordStrength <= 50):
-      const modifiedPassword = modifyPassword(password);
-      return {
-        originalPassword: password,
-        modifiedPassword,
-        passwordStrength,
-        strength: 'weak',
-        message: `${password} is too weak! How about ${modifiedPassword}`
-      }
-    default:
-      return {
-        originalPassword: password,
-        passwordStrength,
-        strength: 'unacceptable',
-        message: `${password} is unacceptable!`
-      }
+  let passwordStrength = countStrength(substitutedString);
+  let returnObj;
+  if (passwordStrength > 50) {
+    return {
+      originalPassword: password,
+      passwordStrength,
+      strength: 'strong',
+      message: `Congratulations ${password} is strong!`
+    }
+  } else if (passwordStrength >= 10 && passwordStrength <= 50){
+    let modifiedPassword = modifyPassword(password);
+    let passwordStrength = countStrength(modifiedPassword);
+    while (passwordStrength <= 50) {
+      modifiedPassword = modifyPassword(modifiedPassword);
+      passwordStrength = countStrength(modifiedPassword);
+    }
+    return {
+      originalPassword: password,
+      modifiedPassword,
+      passwordStrength,
+      strength: 'weak',
+      message: `${password} is too weak! How about ${modifiedPassword}`
+    }
+  } else {
+    return {
+      originalPassword: password,
+      passwordStrength,
+      strength: 'unacceptable',
+      message: `${password} is unacceptable!`
+    }
   }
+  return returnObj;
 };
 
 export function modifyPassword(password) {
@@ -112,5 +109,57 @@ export function modifyPassword(password) {
     repeatedCharacterTypes,
     characterTypes
   ] = countCharacterTypes(password);
-  return repeatedCharacterTypes;
+  if (repeatedCharacterTypes.length && missingCharacterTypes.length) {
+    const replacedString = replaceString(password, repeatedCharacterTypes, missingCharacterTypes);
+    return replacedString;
+  } else {
+    const randomChar = Math.random().toString(36).slice(6)[0];
+    return password + randomChar;
+  }
 };
+
+export function replaceString(password, repeats, missing) {
+  for (let idx = 0; idx < password.length; idx++) {
+    var char = password[idx];
+    var charType = findType(char);
+    if (repeats.includes(charType)) {
+      var missingType = missing.shift()
+      var substitutedString = substituteString(password, idx, missingType);
+      return substitutedString;
+    }
+  }
+}
+
+export function findType(char) {
+  if (char.match(/[a-zA-Z]/g)) {
+    return 'letter';
+  }
+  if (char.match(/[0-9]/g)) {
+    return 'digit';
+  }
+  if (char.match(/ /g)) {
+    return 'whitespace'
+  }
+  return 'other';
+}
+
+export function substituteString(password, idx, missing) {
+  var substitutionMap = {
+    'letter': 'r',
+    'digit': '9',
+    'whitespace': ' ',
+    'other': '.'
+  };
+  var newChar = substitutionMap[missing];
+  var newStr = password.substr(0, idx) + newChar + password.substr(idx+1);
+  console.log("----============")
+  console.log("----============")
+  console.log("----============")
+  console.log("----============")
+  console.log(idx + 1)
+  console.log("----============")
+  console.log("----============")
+  console.log("----============")
+  console.log("----============")
+  return password.substr(0, idx) + newChar + password.substr(idx+1);
+}
--- test/password_service_spec.js
+++ test/password_service_spec.js
@@ -4,18 +4,53 @@ import {
   replaceEnglishWords,
   countStrength,
   countCharacterTypes,
-  modifyPassword
+  modifyPassword,
+  substituteString
 } from '../src/services/password_service';
 
 describe('Password Service', () => {
   describe('validatePassword', () => {
-    // it('', () => {
-    //   expect(validatePassword('lol')).to.equal('lol');
+    //   it('returns the unacceptable password', () => {
+    //     var password = 'a';
+    //     var result = validatePassword(password);
+    //     expect(result.strength).to.equal('unacceptable');
+    //   });
+      it('returns lengthened password when weak', () => {
+        var password = 'x0_ ';
+        var result = validatePassword(password);
+        console.log("=========");
+        console.log("=========");
+        console.log("=========");
+        console.log(result);
+        console.log("=========");
+        console.log("=========");
+        console.log("=========");
+        expect(result.strength).to.equal('weak');
+      });
+    //   it('returns substituted password when weak', () => {
+    //     var password = 'x02)rrrrrrrrrrrr';
+    //
+    //     var result = validatePassword(password);
+    //     expect(result.strength).to.equal('weak');
+    //   });
+    //   it('returns substituted, lengthened password when weak', ()=> {
+    //     var password = 'x02)rrrrrr';
+    //     var result = validatePassword(password);
+    //     expect(result.strength).to.equal('weak');
+    //   });
+    //   it('returns the password unmodified when strong', () => {
+    //     var password = 'x0_ r2461345a';
+    //
+    //     var result = validatePassword(password);
+    //     expect(result.strength).to.equal('strong');
     // });
   });
   describe('replaceWords', () => {
     it('replaces an instance of an english word', () => {
       expect(replaceEnglishWords('password1')).to.equal('a1');
+    });
+    it('replaces passwords with lots of types', () => {
+      expect(replaceEnglishWords('x0_ ')).to.equal('x0_ ');
     });
     it('replaces multiple instances of an english word', () => {
       expect(replaceEnglishWords('password1_goat_why')).to.equal('a1_a_a');
@@ -49,8 +84,22 @@ describe('Password Service', () => {
   });
 
   describe('modifyPassword', () => {
-    it('modifies characters before inserting new ones', () => {
-      expect(modifyPassword('goat m4n')).to.deep.equal(['other']);
+    it('returns a lengthened password', () => {
+      var password = 'x0_ ';
+      var result = modifyPassword(password);
+      console.log(result);
+      expect(result.length).to.not.equal(password.length);
+    });
+    it('returns a substituted before a lengthened password', () => {
+      var password = '.99';
+      var result = modifyPassword(password);
+      expect(result.length).to.equal(password.length);
+    });
+  });
+
+  describe('substituteString', () => {
+    it('substitutes a string with a different charType at an index', () => {
+      expect(substituteString('.99', 1, 'letter')).to.equal('.r9');
     });
   });
 });
